1c3dcaa755592558bcb9803f30d49df2b07dc296
Akira Ueda <ue@gol.com>
2013-05-03 04:06:57 +0900

    add UrlRewriteFilter to redirect from / to /app/

--- src/main/java/net/physalis/javaviews/MyWebAppInitializer.java
+++ src/main/java/net/physalis/javaviews/MyWebAppInitializer.java
@@ -1,35 +1,44 @@
 package net.physalis.javaviews;
 
 import net.physalis.javaviews.web.WebConfig;
-import net.physalis.javaviews.web.jsp.EscapeHtmlElResolver;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
+import java.util.EnumSet;
 
 public class MyWebAppInitializer implements WebApplicationInitializer {
     @Override
-    public void onStartup(ServletContext container) {
-        root(container);
-        web(container);
+    public void onStartup(ServletContext context) {
+        root(context);
+        web(context);
+        urlRewriteFilter(context);
     }
 
 
-    private void root(ServletContext container) {
+    private void root(ServletContext context) {
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
         rootContext.register(AppConfig.class);
-        container.addListener(new ContextLoaderListener(rootContext));
+        context.addListener(new ContextLoaderListener(rootContext));
     }
 
-    private void web(ServletContext container) {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(WebConfig.class);
+    private void web(ServletContext context) {
+        AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+        webContext.register(WebConfig.class);
 
         ServletRegistration.Dynamic servlet =
-                container.addServlet("webServlet", new DispatcherServlet(context));
+                context.addServlet("webServlet", new DispatcherServlet(webContext));
         servlet.addMapping("/app/*");
+    }
+
+    private void urlRewriteFilter(ServletContext context) {
+        FilterRegistration.Dynamic filter = context.addFilter("UrlRewriteFilter", new UrlRewriteFilter());
+        filter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD), false, "/*");
     }
 }
